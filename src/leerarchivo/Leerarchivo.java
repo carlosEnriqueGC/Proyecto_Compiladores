d75fb050578a69ff6b466ce52a21ac76e02220ef
carlos enrique guzman cabrera <cguzmanc10@miumg.edu.gt>
2023-03-22 17:56:48 -0600

    proyecto base agregado

---
+++ src/leerarchivo/Leerarchivo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package leerarchivo;
+
+/**
+ *
+ * @author DELL
+ */
+public class Leerarchivo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       leet ven = new leet();
+       ven.setVisible(true);
+    }
+    
+}
